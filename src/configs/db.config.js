524f401c4233f3adbfc7fc04f061106d5769dba2
ductc241 <tacongduc123@gmail.com>
2021-11-21 23:22:25 +0700

    update

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
  
 async function connect() {   
     try{
-        await mongoose.connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`, {
+        await mongoose.connect('mongodb+srv://admin:utVrYxTjMQiYjgQ8@cluster0.cmtf9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
         });
         console.log('Connect successfully');
     } catch (error) {
-        console.log('Connect failure !!!');
+        console.log(error);
     }
 };
 
